07019110f26c35eb15207445990744a0ff31b2b4
Ryan Chaffee <rchaff7@yahoo.com>
2020-11-15 18:07:20 -0600

    interface

---
+++ SpinningDiscs/BaseDisc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpinningDiscs
+{
+    public abstract class BaseDisc
+    {
+        double StorageSize { get; }
+        string WriteStyle { get; }
+        string ReadStyle { get; }
+        double CurrentCapacity { get; }
+        string DiscType { get; }
+        List<string> Contents { get; set; }
+        string Name { get; set; }
+    }
+}
--- SpinningDiscs/CD.cs
+++ SpinningDiscs/CD.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 namespace SpinningDiscs
 {
-    public class CD
+    public class CD : BaseDisc, IOpticalDisc
     {
-        // TODO: Implement your custom interface.
-
         // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
         //  need to be declared separately. 
+        public void SpinDisc()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
